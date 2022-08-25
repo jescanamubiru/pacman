e559417f59f0f2523c493f01b097535777112009
Silas Kaggwa <silakag@gmail.com>
2022-08-24 23:16:54 -0500

    clean up

--- pacman.js
+++ pacman.js
@@ -4,8 +4,8 @@ var pos = 0;
 let pageWidth = window.innerWidth;
 //This array contains all the PacMan movement images
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ['./images/pacman1.png', './images/pacman2.png'],
+  ['./images/pacman3.png', './images/pacman4.png'],
 ];
 
 // this variable defines what direction should PacMan go into:
